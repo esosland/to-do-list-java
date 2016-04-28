0b730abe6fb64bf150a7d208f87fbf8c21c07e63
Anthony Gold and Elissa Sosland <anthony+elissa@>
2016-04-28 10:49:40 -0700

    add integration root test to link homepage to categories list and add-category form

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,10 @@
 import org.fluentlenium.adapter.FluentTest;
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
@@ -22,53 +21,55 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("Task list!");
+    assertThat(pageSource()).contains("Todo list!");
+    assertThat(pageSource()).contains("View Category List");
+    assertThat(pageSource()).contains("Add a New Category");
   }
 
-  @Test
-  public void taskIsCreatedTest() {
-    goTo("http://localhost:4567/");
-    click("a", withText("Add a new task"));
-    fill("#description").with("Mow the lawn");
-    submit(".btn");
-    assertThat(pageSource()).contains("Your task has been saved.");
-  }
-
-  @Test
-  public void taskIsDisplayedTest() {
-    goTo("http://localhost:4567/tasks/new");
-    fill("#description").with("Mow the lawn");
-    submit(".btn");
-    click("a", withText("View tasks"));
-    assertThat(pageSource()).contains("Mow the lawn");
-  }
-
-  @Test
-  public void multipleTasksAreDisplayedTest() {
-    goTo("http://localhost:4567/tasks/new");
-    fill("#description").with("Mow the lawn");
-    submit(".btn");
-    goTo("http://localhost:4567/tasks/new");
-    fill("#description").with("Buy groceries");
-    submit(".btn");
-    click("a", withText("View tasks"));
-    assertThat(pageSource()).contains("Mow the lawn");
-    assertThat(pageSource()).contains("Buy groceries");
-  }
-
-  @Test
-  public void taskShowPageDisplaysDescription() {
-    goTo("http://localhost:4567/tasks/new");
-    fill("#description").with("Do the dishes");
-    submit(".btn");
-    click("a", withText("View tasks"));
-    click("a", withText("Do the dishes"));
-    assertThat(pageSource()).contains("Do the dishes");
-  }
-
-  @Test
-  public void taskNotFoundMessageShown() {
-    goTo("http://localhost:4567/tasks/999");
-    assertThat(pageSource()).contains("Task not found");
-  }
+//   @Test
+//   public void taskIsCreatedTest() {
+//     goTo("http://localhost:4567/");
+//     click("a", withText("Add a new task"));
+//     fill("#description").with("Mow the lawn");
+//     submit(".btn");
+//     assertThat(pageSource()).contains("Your task has been saved.");
+//   }
+//
+//   @Test
+//   public void taskIsDisplayedTest() {
+//     goTo("http://localhost:4567/tasks/new");
+//     fill("#description").with("Mow the lawn");
+//     submit(".btn");
+//     click("a", withText("View tasks"));
+//     assertThat(pageSource()).contains("Mow the lawn");
+//   }
+//
+//   @Test
+//   public void multipleTasksAreDisplayedTest() {
+//     goTo("http://localhost:4567/tasks/new");
+//     fill("#description").with("Mow the lawn");
+//     submit(".btn");
+//     goTo("http://localhost:4567/tasks/new");
+//     fill("#description").with("Buy groceries");
+//     submit(".btn");
+//     click("a", withText("View tasks"));
+//     assertThat(pageSource()).contains("Mow the lawn");
+//     assertThat(pageSource()).contains("Buy groceries");
+//   }
+//
+//   @Test
+//   public void taskShowPageDisplaysDescription() {
+//     goTo("http://localhost:4567/tasks/new");
+//     fill("#description").with("Do the dishes");
+//     submit(".btn");
+//     click("a", withText("View tasks"));
+//     click("a", withText("Do the dishes"));
+//     assertThat(pageSource()).contains("Do the dishes");
+//   }
+//
+//   @Test
+//   public void taskNotFoundMessageShown() {
+//     goTo("http://localhost:4567/tasks/999");
+//     assertThat(pageSource()).contains("Task not found");
+//   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,29 +1,34 @@
+import java.time.LocalDateTime;
 import org.junit.*;
 import static org.junit.Assert.*;
-import java.time.LocalDateTime;
 
 public class TaskTest {
+  @After
+  public void tearDown() {
+    Task.clear();
+  }
+
   @Test
-  public void Task_instantiatesCorrectly_true() {
+  public void task_instantiatesCorrectly_true() {
     Task myTask = new Task("Mow the lawn");
     assertEquals(true, myTask instanceof Task);
   }
 
   @Test
-  public void Task_instantiatesWithDescription_String() {
+  public void task_instantiatesWithDescription_String() {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
   }
 
   @Test
-  public void isComplete_isFalseAfterInstantiation_false() {
+  public void isCompleted_isFalseAfterInstantiation_false() {
     Task myTask = new Task("Mow the lawn");
     assertEquals(false, myTask.isCompleted());
   }
 
   @Test
   public void getCreatedAt_instantiatesWithCurrentTime_today() {
-    Task myTask = new Task ("Mow the lawn");
+    Task myTask = new Task("Mow the lawn");
     assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
   }
 
@@ -37,27 +42,26 @@ public class TaskTest {
 
   @Test
   public void clear_emptiesAllTasksFromArrayList_0() {
-    Task myTask = new Task ("Mow the lawn");
+    Task myTask = new Task("Mow the lawn");
     Task.clear();
     assertEquals(Task.all().size(), 0);
   }
 
   @Test
-  public void getId_tasksInstantiateWithAnId_1() {
-    Task.clear();
+  public void getId_tasksInstantiateWithAnID_1() {
     Task myTask = new Task("Mow the lawn");
     assertEquals(1, myTask.getId());
   }
 
   @Test
   public void find_returnsTaskWithSameId_secondTask() {
-    Task firstTask = new Task ("Mow the lawn");
+    Task firstTask = new Task("Mow the lawn");
     Task secondTask = new Task("Buy groceries");
     assertEquals(Task.find(secondTask.getId()), secondTask);
-    }
-
-    @Test
-    public void find_returnsNullWhenNoTakFound_null() {
-      assertTrue(Task.find(999) == null);
-    }
   }
+
+  @Test
+  public void find_returnsNullWhenNoTaskFound_null() {
+    assertTrue(Task.find(999) == null);
+  }
+}
